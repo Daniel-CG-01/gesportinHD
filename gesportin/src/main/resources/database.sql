30c1a8549b0f9989336618cbf4719fd0979af9a0
Ianechu <ianechu181005@gmail.com>
2026-01-08 09:10:49 +0100

    Refactor SQL script for Tipoarticulo table creation

--- gesportin/src/main/resources/database.sql
+++ gesportin/src/main/resources/database.sql
@@ -1,45 +1,11 @@
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `Tienda`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `Tipoarticulo`
---
-
 CREATE TABLE `Tipoarticulo` (
   `id` bigint NOT NULL,
   `descripcion` varchar(255) COLLATE utf32_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `Tipoarticulo`
---
 ALTER TABLE `Tipoarticulo`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `Tipoarticulo`
---
 ALTER TABLE `Tipoarticulo`
   MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
